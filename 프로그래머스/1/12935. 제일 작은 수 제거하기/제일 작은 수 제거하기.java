4042f9514d5f702c25cb336ef37ab34ea30e5c3f
taewan <104051002+taewan625@users.noreply.github.com>
2023-10-15 21:19:38 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.07 ms, Memory: 70.1 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -1,10 +1,26 @@
-import static java.util.Arrays.*;
+// import static java.util.Arrays.*;
+import java.util.Arrays;
+
 
 class Solution {
     public int[] solution(int[] arr) {
         if(arr.length == 1) return new int[]{-1};
+        /*
         return stream(arr)
             .filter(factor -> factor!= stream(arr).min().getAsInt())
             .toArray();
+        */
+        
+        int length = arr.length;
+        int minIndex = 0;
+        
+        for(int i = 1 ; i < length ; i++) 
+            minIndex = (arr[minIndex] < arr[i]) ? minIndex : i;
+        
+        int[] answer = new int[length -1];
+        
+        for(int i = 0; i < minIndex; i++) answer[i] = arr[i];
+        for(int i = minIndex; i < length-1; i++) answer[i] = arr[i+1];
+        return answer;
     }
 }
