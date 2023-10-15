01da3fdc0cc147346790ae61b54fc2ada63b4dd1
taewan <104051002+taewan625@users.noreply.github.com>
2023-10-15 20:35:29 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 6.54 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int[] numArr = {0,1,2,3,4,5,6,7,8,9};
+            return Arrays.stream(numArr)
+                    .filter(factor -> Arrays.stream(numbers)
+                            .noneMatch(number -> number == factor))
+                    .sum();
+    }
+}
