3d204804d5e892ce6854ce1da60f8470705be5e4
taewan <104051002+taewan625@users.noreply.github.com>
2024-07-29 23:47:16 +0900

    [level 2] Title: 올바른 괄호, Time: 52.70 ms, Memory: 58.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,35 @@
+class Solution {
+        boolean solution(String s) {
+        //결과값
+        boolean answer = true;
+
+        //유효성 검사 통과 실패 시
+        if(100000 < s.length()) {
+            answer = false;
+
+        }
+        //유효성 검사 통과 시
+        else {
+            int cnt = 0;
+            //문자열1개씩 분리
+            for (String part : s.split("") ) {
+                //-부호가 나오면 바로 false
+                if (cnt < 0) {
+                    answer = false;
+                    break;
+                }
+                
+                //끝까지 갔는데 0이 아니면 false
+                if("(".equals(part)){
+                    cnt++;
+                }
+                else if(")".equals(part)){
+                    cnt--;
+                }
+            }
+            answer = (cnt ==0);
+        }
+
+        return answer;
+    }
+}
